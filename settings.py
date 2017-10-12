e7338119d529ca16e0cea7e4310090c87c49a9b8
huangyiqianlin <1246989343@qq.com>
2017-10-12 22:57:04 +0800

    优化

--- settings.py
+++ settings.py
@@ -7,13 +7,17 @@ class Setting():
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
+        # 背景色
         self.bg_color = (230, 230, 230)
+
+        # 飞船移动速度
         self.ship_speed = 20
 
         self.bullet_speed = 10
-        self.bullet_width = 30
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        # 允许在屏幕上同时出现的子弹数量
         self.bullet_allowed = 10
 
         # 外星人设置
